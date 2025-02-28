1f12ad062a1964a3d9c900d0b948a2b49c6c7b28
Stan Drozd <drozdziak1@gmail.com>
2025-02-28 23:12:03 +0100

    kernel.cl: finish

--- src/kernel.cl
+++ src/kernel.cl
@@ -1,3 +1,83 @@
-kernel void most_common_nseq(uint nseq_size, global const uint *txt, global uint *nseq_counts, global uint * unique_nseqs) {
-   return; 
+kernel void count_unique_nseqs(uint n, global const uint *txt, global uint *nseq_counts, global uint * unique_nseqs, global uint *nseq_slot_locks) {
+    int gid = get_global_id(0);
+    int glob_work_size = get_global_size(0);
+
+    // The last n-1 threads don't do any work
+    if (gid > glob_work_size - n) {
+        return;
+    }
+
+    int nseq_idx = -1;
+    int free_slot_idx = -1;
+
+    // Look for an existing entry for this thread's n-sequence
+    for (int i = 0; i < glob_work_size; ++i) {
+
+        // Wait for a lock if claimed
+        while (atomic_cmpxchg(&nseq_slot_locks[i], 0, 1) != 0) {}
+
+        // We know we've reached the first unoccupied n-sequence slot if its first character is 0
+        if (unique_nseqs[n * i] == 0) {
+            free_slot_idx = i;
+
+            // NOTE: We don't release the lock on purpose, we will
+            // hold onto it until the free slot write stage
+            break;
+        }
+
+        char matches = 0;
+
+        for (int j = 0; j < n; ++j) {
+
+            if (txt[gid + j] == unique_nseqs[n * i + j]) {
+                matches += 1;
+            } else {
+                break;
+            }
+        }
+
+        // release lock
+        atomic_xchg(&nseq_slot_locks[i], 0);
+
+        if (matches == n) {
+            nseq_idx = i;
+            break;
+        }
+    }
+
+    // Skip the write into unique_nseqs if we found a slot occupied by our n-sequence
+    if (nseq_idx != -1) {
+        // No need to lock because of atomic addition
+        atomic_add(&nseq_counts[nseq_idx], 1);
+        return;
+    }
+
+    // Alternatively, a free slot should have been discovered. Write
+    // n-sequence chars to it.
+    if (free_slot_idx != -1) {
+        // Write this thread's n-sequence chars into unique_nseqs
+        for (int j = 0; j < n; ++j) {
+            unique_nseqs[n * free_slot_idx + j] = txt[gid + j];
+        }
+
+        // Set initial value to 1
+        nseq_counts[free_slot_idx] = 1;
+
+        // Release lock
+        atomic_xchg(&nseq_slot_locks[free_slot_idx], 0);
+
+        return;
+    }
+
+    // This section should not be reachable. Indicate a problem with a
+    // magic value in the unused last element
+    nseq_counts[glob_work_size - 1] = 0xdeadbeef;
+
+    if (free_slot_idx != -1) {
+        // Release the lock so that a thread reaching here does not
+        // block other threads forever which makes deadbeef useless.
+        atomic_xchg(&nseq_slot_locks[free_slot_idx], 0);
+    }
+
+    return;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,79 @@
-
 mod nseq_tokenizer;
 
 use ocl::{Buffer, ProQue};
 
 use nseq_tokenizer::ByteTokenizer;
 
-pub static MOST_COMMON_NSEQ_KERNEL: &'static str = include_str!("./kernel.cl");
+pub static COUNT_UNIQUE_NSEQS_KERNEL: &'static str = include_str!("./kernel.cl");
 
 pub type ErrBox = Box<dyn std::error::Error>;
 
-pub const NSEQ_SIZE: usize = 2;
+pub const NSEQ_SIZE: usize = 10;
 
 fn main() -> Result<(), ErrBox> {
+    let s = "rabarbar jest kurwa tak bardzo najsmaczniejszym czymś że o ja pierdolę";
 
-    let s = "rabarbar";
+    let s = format!("{s}{s}{s}{s}{s}");
+    let s = format!("{s}{s}{s}");
 
     let s_preprocessed: Vec<u32> = s.chars().map(|c| c as u32).collect();
 
-    let pro_que = ProQue::builder().src(MOST_COMMON_NSEQ_KERNEL).dims(s_preprocessed.len()).build()?;
+    let pro_que = ProQue::builder()
+        .src(COUNT_UNIQUE_NSEQS_KERNEL)
+        .dims(s_preprocessed.len())
+        .build()?;
 
-    let txt_buf = pro_que.buffer_builder().copy_host_slice(s_preprocessed.as_slice()).build()?;
+    let txt_buf = pro_que
+        .buffer_builder()
+        .copy_host_slice(s_preprocessed.as_slice())
+        .build()?;
     let nseq_counts_buf = pro_que.create_buffer::<u32>()?;
-    let unique_nseqs_buf: Buffer<u32> = pro_que.buffer_builder().len(NSEQ_SIZE * s_preprocessed.len()).build()?;
+    let unique_nseqs_buf: Buffer<u32> = pro_que
+        .buffer_builder()
+        .len(NSEQ_SIZE * s_preprocessed.len())
+        .build()?;
+    let nseq_slot_locks: Buffer<u32> =
+        pro_que.buffer_builder().len(s_preprocessed.len()).build()?;
 
     let kernel = pro_que
-        .kernel_builder("most_common_nseq")
+        .kernel_builder("count_unique_nseqs")
         .arg(NSEQ_SIZE as u32)
         .arg(&txt_buf)
         .arg(&nseq_counts_buf)
         .arg(&unique_nseqs_buf)
+        .arg(&nseq_slot_locks)
         .build()?;
 
     unsafe {
         kernel.enq()?;
     }
 
+    let mut counts: Vec<u32> = vec![0u32; s_preprocessed.len()];
+
+    nseq_counts_buf.read(&mut counts).enq()?;
+
+    println!("counts: {:?}", counts);
+
+    let mut unique_nseqs: Vec<u32> = vec![0u32; NSEQ_SIZE * s_preprocessed.len()];
+
+    unique_nseqs_buf.read(&mut unique_nseqs).enq()?;
+
+    let nseq_strings: Vec<String> = unique_nseqs
+        .chunks(NSEQ_SIZE)
+        .filter_map(|chunk| {
+            if chunk.contains(&0) {
+                None
+            } else {
+                let s: String = chunk
+                    .iter()
+                    .map(|num| char::from_u32(*num).expect("Could not convert u32 to char"))
+                    .collect();
+                Some(s)
+            }
+        })
+        .collect();
+
+    println!("unique_nseqs: {:?}", nseq_strings);
 
     Ok(())
 }
