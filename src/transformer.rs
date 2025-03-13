468c637148ddb4700eeb724b273e11903e88619d
Stan Drozd <drozdziak1@gmail.com>
2025-03-13 16:19:41 +0100

    transformer: randomize weights during init

--- src/transformer.rs
+++ src/transformer.rs
@@ -1,9 +1,18 @@
 use crate::util::ErrBox;
 use ndarray::{Array1, Array2};
+use ndarray_rand::rand_distr::Uniform;
+use ndarray_rand::RandomExt;
 
 pub struct Transformer {
     embed: Array2<f32>,
     blocks: Vec<TransformerBlock>,
+}
+
+/// Computes the value of a min/max tensor randomization value,
+/// e.g. if X = dims2rand_boundary(), you use it for range (-X, X) for
+/// the randomization sampling range.
+fn dims2rand_boundary(x: usize, y: usize) -> f32 {
+    (x as f32 * y as f32).sqrt()
 }
 
 impl Transformer {
@@ -23,7 +32,12 @@ impl Transformer {
             .into());
         }
 
-        let embed = Array2::zeros((ctx_width, embed_dim));
+        let rand_boundary = dims2rand_boundary(ctx_width, embed_dim);
+
+        let embed = Array2::random(
+            (ctx_width, embed_dim),
+            Uniform::new(-rand_boundary, rand_boundary),
+        );
 
         let blocks = (0..num_layers)
             .map(|_i| TransformerBlock::new(embed_dim, num_heads, ff_dim, dropout))
@@ -43,13 +57,17 @@ pub struct TransformerBlock {
 
 impl TransformerBlock {
     pub fn new(embed_dim: usize, num_heads: usize, ff_dim: usize, dropout: f32) -> Self {
-        let query = Array2::zeros((embed_dim, embed_dim));
-        let key = Array2::zeros((embed_dim, embed_dim));
-        let value = Array2::zeros((embed_dim, embed_dim));
+        let rand_boundary = dims2rand_boundary(embed_dim, embed_dim);
+        let rand_dist = Uniform::new(-rand_boundary, rand_boundary);
 
-        let ln = Array1::zeros(embed_dim);
+        let query = Array2::random((embed_dim, embed_dim), rand_dist);
+        let key = Array2::random((embed_dim, embed_dim), rand_dist);
+        let value = Array2::random((embed_dim, embed_dim), rand_dist);
 
-        let out = Array2::zeros((embed_dim, embed_dim));
+	let ln_rand_boundary = dims2rand_boundary(1, embed_dim);
+        let ln = Array1::random(embed_dim, Uniform::new(-ln_rand_boundary, ln_rand_boundary));
+
+        let out = Array2::random((embed_dim, embed_dim), rand_dist);
         Self {
             query,
             key,
