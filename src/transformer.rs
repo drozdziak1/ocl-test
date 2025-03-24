4fd5f106dea39c7858fdb580823ca382a0860875
Stan Drozd <drozdziak1@gmail.com>
2025-03-24 21:18:36 +0100

    transformer: WIP backprop

--- src/transformer.rs
+++ src/transformer.rs
@@ -82,7 +82,9 @@ pub struct TransformerTrainData {
     tb_train_data: Vec<TBTrainData>,
     /// last row of X after all transformer blocks add their output to it
     last_refined_embedding: Array2<f32>,
-    /// softmax(last_refined_embedding)
+    /// last_refined_embedding @ unembed
+    pred_raw: Array2<f32>,
+    /// softmax(pred_raw)
     prediction: Array2<f32>,
 }
 
@@ -136,10 +138,13 @@ impl Transformer {
                 embedded_row.assign(&embedding);
             });
 
-        let mut tb_train_data_iter = train_data
-            .iter_mut()
-            .map(|train_data| train_data.tb_train_data.iter_mut())
-            .flatten();
+        let mut tb_train_data = if train_data.is_some() {
+            vec![Default::default(); self.blocks.len()]
+        } else {
+            vec![]
+        };
+
+        let mut tb_train_data_iter = tb_train_data.iter_mut();
 
         for block in self.blocks.iter() {
             block.naive_fwd(&mut x_embedded, tb_train_data_iter.next())?;
@@ -148,28 +153,65 @@ impl Transformer {
         let last_refined_embedding = x_embedded
             .axis_iter(Axis(0))
             .next_back()
-            .ok_or_else(|| "Could not get last element of X!")?.to_owned();
+            .ok_or_else(|| "Could not get last element of X!")?
+            .to_owned();
 
         let last_refined_embedding = last_refined_embedding.insert_axis(Axis(0));
 
-        // prediction = softmax(x[last; ..] @ self.unembed)
-        let prediction = softmax_axis(&last_refined_embedding.dot(&self.unembed), Axis(0));
+        // pred_raw = x[last] @ self.unembed
+        let pred_raw = &last_refined_embedding.dot(&self.unembed);
 
-	if let Some(train_data) = train_data {
-	    train_data.x_embedded = x_embedded;
-	    train_data.last_refined_embedding = last_refined_embedding;
-	    train_data.prediction = prediction.clone();
-	}
+        // prediction = softmax(pred_raw)
+        let prediction = softmax_axis(&pred_raw, Axis(0));
+
+        if let Some(train_data) = train_data {
+            train_data.x_embedded = x_embedded;
+            train_data.tb_train_data = tb_train_data;
+            train_data.last_refined_embedding = last_refined_embedding;
+            train_data.pred_raw = pred_raw.clone();
+            train_data.prediction = prediction.clone();
+        }
 
         Ok(prediction.remove_axis(Axis(0)))
     }
 
     pub fn naive_bwd(
-        &self,
+        &mut self,
         lr: f32,
-        y_hat: &Array1<f32>,
+        train_data: &TransformerTrainData,
         y_gt: &Array1<f32>,
     ) -> Result<(), ErrBox> {
+        let z = &train_data.pred_raw;
+        let p = &train_data.prediction;
+        let loss = -(y_gt * p.log10()).sum();
+
+        // dbg!(&loss);
+
+        let grad_l_z = p - y_gt;
+
+        // dbg!(&grad_l_z);
+
+        let grad_l_unembed = train_data.last_refined_embedding.t().dot(&grad_l_z);
+
+        // dbg!(&grad_l_unembed);
+
+        let grad_l_h = grad_l_z.dot(&self.unembed.t());
+
+        dbg!(train_data.tb_train_data.len());
+
+        let tb_iter = self
+            .blocks
+            .iter_mut()
+            .zip(train_data.tb_train_data.iter())
+            .rev();
+
+        let mut grad_block_output = grad_l_h;
+        for (tb, tb_train_data) in tb_iter {
+            grad_block_output = tb.naive_bwd(tb_train_data, grad_block_output)?;
+        }
+
+        panic!("kupak");
+
         Ok(())
     }
 }
@@ -181,6 +223,7 @@ pub struct TransformerBlock {
     query: Array3<f32>,
     key: Array3<f32>,
     value: Array3<f32>,
+    atn_out: Array2<f32>,
     ff1: Array2<f32>,
     ff1_b: Array2<f32>,
     ff2: Array2<f32>,
@@ -188,7 +231,7 @@ pub struct TransformerBlock {
 }
 
 /// Intermediate state of X as it goes through the TransformerBlock; used for training
-#[derive(Default)]
+#[derive(Clone, Default)]
 pub struct TBTrainData {
     /// X @ Q per attention head
     x_qs: Array3<f32>,
@@ -199,7 +242,11 @@ pub struct TBTrainData {
     /// Xq @ Xk per attention head
     xqxk: Array3<f32>,
     /// XqXk @ Xv, all attention heads
-    atn_out_reshaped: Array2<f32>,
+    atn_scores_reshaped: Array2<f32>,
+    /// atn_scores_reshaped @ atn_out
+    atn_scores_atn_out: Array2<f32>,
+    /// X + atn_out_reshaped
+    x_with_atn_out: Array2<f32>,
     /// (X + atn_out_reshaped) @ ff1
     x_ff1: Array2<f32>,
     /// Xff1 + ff1_b
@@ -210,10 +257,8 @@ pub struct TBTrainData {
     x_ff2: Array2<f32>,
     /// Xff2 + ff2_b
     x_ff2_b: Array2<f32>,
-    /// GELU(Xff2_b)
-    x_ff2_act: Array2<f32>,
-    /// X + atn_out_reshape + X_ff2_act
-    x_out: Array2<f32>,
+    /// X + atn_out_reshape + X_ff2_b
+    x_with_ff: Array2<f32>,
 }
 
 impl TransformerBlock {
@@ -229,6 +274,10 @@ impl TransformerBlock {
         let key = Array3::random(qkv_dims, qkv_rand_dist);
 
         let value = Array3::random(qkv_dims, qkv_rand_dist);
+
+        let atn_out_rand_dist = dims2rand_dist(vec![embed_dim, embed_dim].as_slice());
+
+        let atn_out = Array2::random((embed_dim, embed_dim), atn_out_rand_dist);
 
         let ff_rand_dist = dims2rand_dist(vec![embed_dim, ff_dim].as_slice());
 
@@ -246,6 +295,8 @@ impl TransformerBlock {
             query,
             key,
             value,
+            atn_out,
+
             ff1,
             ff1_b,
             ff2,
@@ -321,10 +372,10 @@ impl TransformerBlock {
                 xqxk_slice.assign(&normalized);
             });
 
-        let mut atn_out: Array3<f32> =
+        let mut atn_scores: Array3<f32> =
             Array3::zeros((self.num_heads, x_shp[0].len, self.head_size));
 
-        Zip::from(atn_out.outer_iter_mut())
+        Zip::from(atn_scores.outer_iter_mut())
             .and(xqxk.outer_iter())
             .and(x_vs.outer_iter())
             .par_for_each(|mut atn_out_slice, xqxk_slice, x_vs_slice| {
@@ -332,23 +383,27 @@ impl TransformerBlock {
             });
 
         // (num_heads, T, head_size) -> (T, C)
-        let mut atn_out_reshaped: Array2<f32> = Array2::zeros((x_shp[0].len, x_shp[1].len));
+        let mut atn_scores_reshaped: Array2<f32> = Array2::zeros((x_shp[0].len, x_shp[1].len));
 
-        Zip::from(atn_out_reshaped.rows_mut())
-            .and(atn_out.axis_iter(Axis(1)))
-            .par_for_each(|mut reshaped_row, atn_out_slice| {
-                let new_row = atn_out_slice
+        Zip::from(atn_scores_reshaped.rows_mut())
+            .and(atn_scores.axis_iter(Axis(1)))
+            .par_for_each(|mut reshaped_row, atn_scores_slice| {
+                let new_row = atn_scores_slice
                     .to_shape(((x_shp[1].len), Order::RowMajor))
                     .expect(&format!(
                         "Could not reshape {:?} to {:?}",
-                        atn_out_slice.dim(),
+                        atn_scores_slice.dim(),
                         (1, x_shp[1].len)
                     ));
 
                 reshaped_row.assign(&new_row);
             });
 
-        *x += &atn_out_reshaped;
+        let atn_scores_atn_out: Array2<f32> = atn_scores_reshaped.dot(&self.atn_out);
+
+        let x_with_atn_out = &*x + &atn_scores_atn_out;
+
+        *x = x_with_atn_out.clone();
 
         // MLP
 
@@ -365,7 +420,7 @@ impl TransformerBlock {
 
         let x_ff1_act: Array2<f32> = approx_gelu(&x_ff1_b);
 
-        // Xff2 = GELU(Xff1 @ ff2 + ff2_b)
+        // Xff2 = Xff1 @ ff2 + ff2_b
         let x_ff2_dims = (x_shp[0].len, x_shp[1].len);
         let ff2_b_broadcast = self
             .ff2_b
@@ -379,15 +434,133 @@ impl TransformerBlock {
 
         let x_ff2_b: Array2<f32> = &x_ff2 + &ff2_b_broadcast;
 
-        let x_ff2_act: Array2<f32> = approx_gelu(&x_ff2_b);
+        let x_with_ff = &*x + &x_ff2_b;
 
-        *x += &x_ff2_act;
+        *x = x_with_ff.clone();
+
+        if let Some(train_data) = train_data {
+            *train_data = TBTrainData {
+                x_qs,
+                x_ks,
+                x_vs,
+                xqxk,
+                atn_scores_reshaped,
+                atn_scores_atn_out,
+                x_with_atn_out,
+                x_ff1,
+                x_ff1_b,
+                x_ff1_act,
+                x_ff2,
+                x_ff2_b,
+                x_with_ff,
+            }
+        }
 
         Ok(())
     }
 
-    pub fn naive_bwd(&mut self) -> Result<(), ErrBox> {
-        Ok(())
+    pub fn naive_bwd(
+        &mut self,
+        train_data: &TBTrainData,
+        grad_l_block_out: Array2<f32>,
+    ) -> Result<Array2<f32>, ErrBox> {
+        // FEEDFORWARD
+
+        // a.k.a. dL/dx_with_ff
+        let grad_l_hffn = grad_l_block_out;
+
+        // Final token's ff1 activated output (from chain rule: d(a*b) / d_b = a, d(a*b) / d_a = b)
+        let last_x_ff1_act = train_data
+            .x_ff1_act
+            .axis_iter(Axis(0))
+            .next_back()
+            .ok_or_else(|| "Could not get last element of x_ff1_act")?
+            .to_owned()
+            .insert_axis(Axis(0));
+
+        let grad_l_ff2 = last_x_ff1_act.t().dot(&grad_l_hffn);
+
+        let grad_l_ff2_b = grad_l_hffn.clone();
+
+        dbg!(&grad_l_ff2);
+
+        let grad_l_ff1_act = grad_l_hffn.dot(&self.ff2.t());
+
+        dbg!(&grad_l_ff1_act);
+
+        let last_x_ff1_b = train_data
+            .x_ff1_b
+            .axis_iter(Axis(0))
+            .next_back()
+            .ok_or_else(|| "Could not get last element of x_ff1_b")?
+            .to_owned()
+            .insert_axis(Axis(0));
+
+        // aka dL/dx_ff1_b
+        let grad_l_hprime = grad_l_ff1_act * approx_gelu_grad(&last_x_ff1_b);
+
+        let last_x_with_atn_out = train_data
+            .x_with_atn_out
+            .axis_iter(Axis(0))
+            .next_back()
+            .ok_or_else(|| "Could not get last element of x_with_atn_out")?
+            .to_owned()
+            .insert_axis(Axis(0));
+
+        let grad_l_ff1 = last_x_with_atn_out.t().dot(&grad_l_hprime);
+
+        dbg!(&grad_l_ff1);
+
+        let grad_l_ff1_b = grad_l_hprime.clone();
+
+        dbg!(&grad_l_ff1_b);
+
+        // aka dL/dx_with_atn_out
+        let grad_l_hatn = grad_l_hprime.dot(&self.ff1.t());
+
+        dbg!(&grad_l_hatn);
+
+        // FINAL atn_out MATRIX
+
+        let last_atn_score = train_data
+            .atn_scores_reshaped
+            .axis_iter(Axis(0))
+            .next_back()
+            .ok_or_else(|| "Could not get last element of atn_scores_atn_out")?
+            .to_owned()
+            .insert_axis(Axis(0));
+
+        let grad_l_atn_out = last_atn_score.t().dot(&grad_l_hatn);
+
+        dbg!(&grad_l_atn_out);
+
+        // aka dL/datn_scores_atn_out
+        let grad_l_o = grad_l_hatn.dot(&self.atn_out.t());
+
+        let grad_l_o_shp: Vec<_> = grad_l_o.axes().collect();
+
+        // Split for individual heads, i.e. (T, C) -> (num_heads, T, head_size)
+        let mut grad_l_o_reshaped: Array3<f32> =
+            Array3::zeros((self.num_heads, grad_l_o_shp[0].len, self.head_size));
+
+        Zip::from(grad_l_o_reshaped.axis_iter_mut(Axis(1)))
+            .and(grad_l_o.rows())
+            .par_for_each(|mut reshaped_row, grad_l_o_row| {
+                let new_row = grad_l_o_row
+                    .to_shape(((self.num_heads, self.head_size), Order::RowMajor))
+                    .expect(&format!(
+                        "Could not reshape {:?} to {:?}",
+                        grad_l_o_row.dim(),
+                        (self.num_heads, self.head_size)
+                    ));
+                reshaped_row.assign(&new_row);
+            });
+
+        // aka dL/dxqxk
+        let mut grad_l_a: Array3<f32> = Array3::zeros(train_data.xqxk.dim());
+        let mut grad_l_xvs: Array3<f32> = Array3::zeros(train_data.x_vs.dim());
+
+        Ok(grad_l_hffn)
     }
 }
 
@@ -420,12 +593,17 @@ mod tests {
     #[test]
     fn test_transformer_naive_fwd_happy_path() -> Result<(), ErrBox> {
         let ctx_size = 32_000;
-        let embed_dim = 32;
         let vocab_size = 15;
+        let embed_dim = 32;
+        let num_layers = 32;
+        let num_heads = 16;
+        let ff_dim = 18;
 
-        let t = Transformer::new(ctx_size, vocab_size, embed_dim, 32, 16, 18)?;
+        let mut t = Transformer::new(
+            ctx_size, vocab_size, embed_dim, num_layers, num_heads, ff_dim,
+        )?;
 
-	let mut train_data = TransformerTrainData::default();
+        let mut train_data = TransformerTrainData::default();
 
         let pred = t.naive_fwd(vec![1, 2, 3, 4, 5].as_slice(), Some(&mut train_data))?;
 
@@ -433,7 +611,7 @@ mod tests {
         let mut expected = Array1::zeros(vocab_size);
         expected[5] = 1.0;
 
-        t.naive_bwd(0.001, &pred, &expected)?;
+        t.naive_bwd(0.001, &train_data, &expected)?;
 
         Ok(())
     }
